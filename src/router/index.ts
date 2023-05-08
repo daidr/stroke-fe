55c5155fa7bd646ed555f3b2e35e69c3fd01a169
daidr <daidr@daidr.me>
2023-05-08 23:03:37 +0800

    feat: router guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createWebHistory, createRouter } from 'vue-router'
+import { useDoctorStore } from '@/stores/doctor'
 
 const routeModules = import.meta.globEager('../app/**/routes/index.js')
 
@@ -19,6 +20,25 @@ for (const element in routeModules) {
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  const authType = to.meta.authType
+
+  if (!authType) {
+    next()
+    return
+  } else if (authType === 'doctor') {
+    const doctorStore = useDoctorStore()
+    if (!doctorStore.isLoggedIn) {
+      const result = await doctorStore.refreshDoctorInfo()
+      if (!result) {
+        next({ path: '/login' })
+        return
+      }
+    }
+  }
+  next()
 })
 
 router.afterEach((to, from, failure) => {
