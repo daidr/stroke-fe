5e1d569f7aea317ccd76a931e892b1c7600d5e09
daidr <daidr@daidr.me>
2023-05-08 23:05:58 +0800

    feat: message container

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <script setup lang="ts">
 import { RouterView, useRoute } from 'vue-router'
 import MainHeader from '@/components/MainHeader.vue'
-
+import MessageContainer from '@/components/Message/MessageContainer.vue'
 const route = useRoute()
 </script>
 
 <template>
   <MainHeader v-if="!route.meta.hideHeader" />
   <RouterView />
+  <MessageContainer />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
