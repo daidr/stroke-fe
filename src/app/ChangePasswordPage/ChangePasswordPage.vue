4b38604a065418344861de56f479f6a44ac9fc71
daidr <daidr@daidr.me>
2023-05-15 10:31:09 +0800

    feat: change password

---
+++ src/app/ChangePasswordPage/ChangePasswordPage.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { changeDoctorPassword } from '@/api/doctor/auth'
+import { useMessage } from '@/components/Message'
+const router = useRouter()
+const oldPassword = ref('')
+const newPassword = ref('')
+const repeatPassword = ref('')
+
+const isLoading = ref(false)
+
+const { error, success } = useMessage()
+
+const changepw = async () => {
+  if (!oldPassword.value || !newPassword.value || !repeatPassword.value) {
+    error('密码不嫩为空')
+    return
+  }
+  if (newPassword.value != repeatPassword.value) {
+    error('两次输入的密码不一致')
+    return
+  }
+
+  isLoading.value = true
+
+  const result = await changeDoctorPassword(oldPassword.value, newPassword.value)
+  if (result) {
+    router.push('/home')
+    success('密码修改成功')
+  }
+  isLoading.value = false
+}
+</script>
+
+<template>
+  <div class="changepw-wrapper">
+    <div class="form" :class="{ loading: isLoading }">
+      <input v-model="oldPassword" type="current-password" placeholder="现在的密码" />
+      <input v-model="newPassword" type="new-password" placeholder="新密码" />
+      <input v-model="repeatPassword" type="repeat-password" placeholder="重复新密码" />
+      <div class="actions">
+        <div class="changepw-btn" @click="changepw">修改密码</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.changepw-wrapper {
+  @apply fixed top-15 left-0 right-0 bottom-0;
+  @apply flex items-center justify-center flex-col;
+  @apply space-y-10;
+
+  .form {
+    @apply flex flex-col space-y-2;
+
+    .actions {
+      @apply flex items-center justify-between;
+
+      a {
+        @apply text-dark/70;
+      }
+    }
+
+    input {
+      @apply px-3 py-2 text-lg;
+      @apply border-0 ring-1 ring-dark/70 rounded-md outline-none;
+    }
+
+    .changepw-btn {
+      @apply px-3 py-2;
+      @apply border-0 ring-1 ring-dark/70 rounded-md text-dark/70;
+      @apply cursor-pointer;
+      @apply transition;
+      &:hover {
+        @apply bg-dark/70 text-white;
+      }
+      &:active {
+        @apply opacity-80;
+      }
+    }
+
+    &.loading {
+      @apply opacity-50 pointer-events-none;
+    }
+  }
+}
+</style>
---
+++ src/app/ChangePasswordPage/routes/index.js
@@ -0,0 +1,9 @@
+export const routes = [
+  {
+    path: '/changepw',
+    component: () => import('../ChangePasswordPage.vue'),
+    meta: {
+      pageTitle: '修改密码'
+    }
+  }
+]
