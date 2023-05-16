687a516085b071462949992722d893964df1b07f
daidr <daidr@daidr.me>
2023-05-16 20:37:05 +0800

    feat: utils

--- src/utils/_.ts
+++ src/utils/_.ts
@@ -8,3 +8,40 @@ export const getGenderString = (genderIdentifier: number) => {
       return '其他'
   }
 }
+
+export const getStrokeTypeString = (strokeType: number) => {
+  switch (strokeType) {
+    case 1:
+      return '出血性脑卒中'
+    case 2:
+      return '缺血性脑卒中'
+    default:
+      return '其他'
+  }
+}
+
+export const getStrokeLevelString = (strokeLevel: number) => {
+  switch (strokeLevel) {
+    case 1:
+      return '轻微'
+    case 2:
+      return '中度'
+    case 3:
+      return '严重'
+    default:
+      return '未知'
+  }
+}
+
+export const getStrokeLevelColor = (strokeLevel: number) => {
+  switch (strokeLevel) {
+    case 1:
+      return 'bg-yellow-500'
+    case 2:
+      return 'bg-orange-400'
+    case 3:
+      return 'bg-red-700'
+    default:
+      return 'bg-gray-400'
+  }
+}
