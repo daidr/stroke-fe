a2fe6fded34482f32866255f4fafc581fb70f288
daidr <daidr@daidr.me>
2023-05-16 20:24:05 +0800

    feat(DiagnosisItem): dark style

--- src/components/DiagnosisItem.vue
+++ src/components/DiagnosisItem.vue
@@ -18,7 +18,7 @@ defineProps({
   <div>
     <div class="flex">
       <div class="flex-grow">
-        <div class="text-sm pb-2 text-black/30">
+        <div class="text-sm pb-2 text-black/30 dark:text-white/30">
           {{ new Date(item.diagnosis_date).toLocaleDateString() }}
         </div>
         <div v-if="name" class="name">姓名：{{ name }}</div>
@@ -26,7 +26,7 @@ defineProps({
       </div>
       <div class="flex-shrink-0 flex items-center">
         <div class="rounded-md text-sm px-1 mr-1 text-white bg-red">严重</div>
-        <div class="text-black/40 text-sm">出血性脑卒中</div>
+        <div class="text-black/40 dark:text-white/40 text-sm">出血性脑卒中</div>
       </div>
     </div>
   </div>
